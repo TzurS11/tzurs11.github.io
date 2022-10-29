086616168d457f4602e398e713238307eed9db93
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2022-10-29 14:49:02 +0300

    maps

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 fetch("https://api.beatsaver.com/maps/uploader/4284455/0")
   .then((req) => req.json())
   .then((res) => {
+    let sum = [0, 0];
     res.docs.forEach((map) => {
-      let div = document.createElement("p");
-      div.innerHTML = `<a class="map-list"target="_blank" href="https://beatsaver.com/maps/${map.id}"><span class = "bold">&#8226</span> ${map.name}</a>`;
-      div.className = "map-list";
-      document.getElementById("maps").appendChild(div);
+      let tr = document.createElement("tr");
+      tr.className = "maps";
+      tr.innerHTML = `<td>${map.name}</td><td>${map.stats.upvotes}</td><td>${map.stats.downvotes}</td><td><a class="link"href="beatsaver://${map.id}">${map.id}<a></td>`;
+      document.getElementById("map-table").appendChild(tr);
+      sum[0] += map.stats.upvotes;
+      sum[1] += map.stats.downvotes;
     });
+    // let tr = document.createElement("tr");
+    // tr.className = "maps";
+    // tr.innerHTML = `<td></td><td>${sum[0]}</td><td>${sum[1]}</td><td></td>`;
+    // document.getElementById("map-table").appendChild(tr);
   });
