ef00011197263004d20d562673a3d0a979044e1b
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2022-10-29 20:07:48 +0300

    table and function funny
    wont let you view table until the maps are loaded into the table

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ function tableMap() {
   let tableTitle = document.getElementById("map-title");
   if (table.style.display === "none") {
     table.style.display = "inline-table";
-    tableTitle.innerHTML = 'Beat saber maps <span class="scaleArrow">▼</span>';
+    tableTitle.title = "Hide maps";
+    tableTitle.innerHTML = 'Beat Saber Maps <span class="scaleArrow">▼</span>';
   } else {
     table.style.display = "none";
-    tableTitle.innerHTML = 'Beat saber maps <span class="scaleArrow">►</span>';
+    tableTitle.title = "Show maps";
+    tableTitle.innerHTML = 'Beat Saber Maps <span class="scaleArrow">►</span>';
   }
 }
+
+function calcRating(upvotes, downvotes, rounding = 1) {
+  let totalReviews = upvotes + downvotes;
+  let reviewScore = upvotes / totalReviews;
+  return Number(
+    (
+      (reviewScore -
+        (reviewScore - 0.5) * Math.pow(2, -Math.log10(totalReviews + 1))) *
+      100
+    ).toFixed(rounding)
+  );
+}
+
 fetch("https://api.beatsaver.com/maps/uploader/4284455/0")
   .then((req) => req.json())
   .then((res) => {
@@ -18,25 +33,35 @@ fetch("https://api.beatsaver.com/maps/uploader/4284455/0")
       orderedMaps.push(map);
     });
     orderedMaps.sort(function (a, b) {
-      return b.stats.upvotes - a.stats.upvotes;
+      return (
+        calcRating(b.stats.upvotes, b.stats.downvotes, 5) -
+        calcRating(a.stats.upvotes, a.stats.downvotes, 5)
+      );
     });
+    let maxmaps = 0;
     orderedMaps.forEach((map) => {
-      let tr = document.createElement("tr");
-      tr.className = "maps";
-      tr.innerHTML = `<td><img class="coverImage" src="${
-        map.versions[map.versions.length - 1].coverURL
-      }"> <span class="map-table-title">${map.name}</span></td><td>${map.stats.upvotes}</td><td>${
-        map.stats.downvotes
-      }</td><td><a class="link" title="One-Click install" href="beatsaver://${
-        map.id
-      }">${map.id}<a></td>`;
-      document.getElementById("map-table").appendChild(tr);
-      sum[0] += map.stats.upvotes;
-      sum[1] += map.stats.downvotes;
+      if (maxmaps < 7) {
+        maxmaps++;
+        let tr = document.createElement("tr");
+        tr.className = "maps";
+        tr.innerHTML = `<td><img class="coverImage" src="${
+          map.versions[map.versions.length - 1].coverURL
+        }"> <span class="map-table-title">${
+          map.name
+        }</span></td><td>${calcRating(
+          map.stats.upvotes,
+          map.stats.downvotes
+        )}%</td><td><a class="link" title="One-Click install" href="beatsaver://${
+          map.id
+        }">${map.id}<a></td>`;
+        document.getElementById("map-table").appendChild(tr);
+        sum[0] += map.stats.upvotes;
+        sum[1] += map.stats.downvotes;
+      }
     });
-    // let tr = document.createElement("tr");
-    // tr.className = "maps";
-    // tr.innerHTML = `<td></td><td>${sum[0]}</td><td>${sum[1]}</td><td></td>`;
-    // document.getElementById("map-table").appendChild(tr);
-    document.body.style.filter = "blur(0px)";
+    let title = document.getElementById("map-title");
+    title.onclick = tableMap;
+    title.style.display = "inline";
+    title.style.cursor = "pointer";
+    title.innerHTML = 'Beat Saber Maps <span class="scaleArrow">►</span>';
   });
