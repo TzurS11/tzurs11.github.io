7dd3763c8d247942a1beeb46470f2352b53f9bb9
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2022-10-29 15:13:45 +0300

    funny easter egg

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+function onNamePress() {
+  colorChange = setInterval(function () {
+    document.body.style.backgroundColor =
+      "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+  }, 10);
+}
 fetch("https://api.beatsaver.com/maps/uploader/4284455/0")
   .then((req) => req.json())
   .then((res) => {
@@ -5,7 +11,7 @@ fetch("https://api.beatsaver.com/maps/uploader/4284455/0")
     res.docs.forEach((map) => {
       let tr = document.createElement("tr");
       tr.className = "maps";
-      tr.innerHTML = `<td>${map.name}</td><td>${map.stats.upvotes}</td><td>${map.stats.downvotes}</td><td><a class="link"href="beatsaver://${map.id}">${map.id}<a></td>`;
+      tr.innerHTML = `<td>${map.name}</td><td>${map.stats.upvotes}</td><td>${map.stats.downvotes}</td><td><a class="link" title="One-Click install" href="beatsaver://${map.id}">${map.id}<a></td>`;
       document.getElementById("map-table").appendChild(tr);
       sum[0] += map.stats.upvotes;
       sum[1] += map.stats.downvotes;
@@ -14,4 +20,5 @@ fetch("https://api.beatsaver.com/maps/uploader/4284455/0")
     // tr.className = "maps";
     // tr.innerHTML = `<td></td><td>${sum[0]}</td><td>${sum[1]}</td><td></td>`;
     // document.getElementById("map-table").appendChild(tr);
+    document.body.style.filter = "blur(0px)";
   });
