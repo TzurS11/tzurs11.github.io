eabb9b6223d36f9e781626a0ad22018e57295f18
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2022-11-12 12:04:46 +0200

    even better

--- 404.js
+++ 404.js
@@ -1,7 +1,7 @@
 if (
   window.location.href
     .toLowerCase()
-    .startsWith("https://tzurs11.github.io/LatestBeats".toLowerCase())
+    .includes("tzurs11.github.io/LatestBeats".toLowerCase())
 ) {
   window.location.href = "https://tzurs11.github.io/LatestBeats";
 }
