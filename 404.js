51fd8eee6caaf02c140b9a7a3bb0ea97822a20b8
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2022-11-12 12:15:08 +0200

    removed shit

--- 404.js
+++
@@ -1,7 +0,0 @@
-if (
-  window.location.href
-    .toLowerCase()
-    .includes("tzurs11.github.io/LatestBeats".toLowerCase())
-) {
-  window.location.href = "https://tzurs11.github.io/LatestBeats";
-}
